04da2c87cfa04fb26d312bee34ee7a4e94cd00d5
Matthieu VERGNE <matthieu.vergne@gmail.com>
2011-09-16 03:29:05 +0200

    Write the Javadoc of the script space builder.

--- cellularautomaton-core/src/main/java/org/cellularautomaton/space/builder/ScriptSpaceBuilder.java
+++ cellularautomaton-core/src/main/java/org/cellularautomaton/space/builder/ScriptSpaceBuilder.java
@@ -19,6 +19,7 @@ import java.util.Map;
 import org.cellularautomaton.cell.ICell;
 import org.cellularautomaton.rule.DynamicRule;
 import org.cellularautomaton.rule.DynamicRule.RulePart;
+import org.cellularautomaton.rule.StaticRule;
 import org.cellularautomaton.space.ISpace;
 import org.cellularautomaton.space.builder.expression.Expression;
 import org.cellularautomaton.space.builder.expression.ExpressionHelper;
@@ -28,7 +29,220 @@ import org.cellularautomaton.util.Coords;
 import org.cellularautomaton.util.Switcher;
 
 /**
- * A file space builder allows to create a space from a description file.
+ * <p>
+ * A file space builder allows to create a space from a description. Here is an
+ * example of description :
+ * 
+ * <pre>
+ * [config]
+ * states=ABC
+ * [rule]
+ * (0,0)=A : B
+ * (-1,0)+(+1,0)+(0,-1)+(0,+1)=4B : A
+ * (0,0)=B & ((0,1)=A | (0,-1)=A) : C
+ * [cells]
+ * AAAAAAAAAAAAAAAA
+ * AAABBBBBCCCCCAAA
+ * AAAAAAAAAAAAAAAA
+ * </pre>
+ * 
+ * </p>
+ * 
+ * <h2>Config</h2>
+ * 
+ * <p>
+ * The configuration part allows to configure some properties of the space. In
+ * particular :
+ * <ul>
+ * <li><code>states</code> : this property gives the possible states of the
+ * cells, the first one being the default state (the one applied to all the
+ * cells by default).</li>
+ * </ul>
+ * </p>
+ * 
+ * <h2>Rule</h2>
+ * 
+ * <p>
+ * The rule part describes the rules to use in the automaton. Each line of the
+ * rule part as to describe a condition to change the state of the cell, meaning
+ * that the cell will just keep its value if no rule matches. Because of that,
+ * the rule part can be entirely omitted (meaning no cell will evolve, like a
+ * {@link StaticRule}) without generate any exception.
+ * </p>
+ * <p>
+ * The example here describe several kind of rules. In order to understand them
+ * well, you have to remember that the same rules are applied to each cell.
+ * Especially, the coordinates used are relative coordinates regarding the
+ * current cell. Here are some explanations about the rules used in the example
+ * :
+ * <ol>
+ * <li><code>(0,0)=A : B</code><br/>
+ * This line is the simplest rule, just saying that if the current cell (the
+ * cell at the coordinates (0,0) regarding the current cell, so the current cell
+ * itself) has the state A, then it take the state B. More generally, each rule
+ * follow this format :<br/>
+ * 
+ * <pre>
+ * &lt;condition&gt; : &lt;new state&gt;
+ * </pre>
+ * 
+ * where <i>condition</i> is a formula as we describe it here, and <i>new
+ * state</i> is the next state of the cell if the condition is matched.</li>
+ * 
+ * <li><code>(-1,0)+(+1,0)+(0,-1)+(0,+1)=4B : A</code><br/>
+ * This line shows the complete form of the previous line. In fact, the
+ * comparison between cells and states is not limited to one. This is a set of
+ * cells compared to a set of constraints. In our example, we consider the four
+ * direct neighbors of the current cell (
+ * <code>(-1,0)+(+1,0)+(0,-1)+(0,+1)</code>) and check this set of cells
+ * contains exactly four cells with the state B (<code>4B</code>) meaning they
+ * are all with the state B. The other states are not constrained here, but for
+ * example the constraint can be <code>2B</code>, meaning that two cells in the
+ * set should have the state B and the others should have another state (A or
+ * C), or <code>2B+1A+1C</code>, meaning we want exactly two cells with B, one
+ * with A and one with C. For the last one, we can also write <code>2B+1A</code>
+ * , as the last cell can have any state other than B or A, so only C in our
+ * case. Of course, asking for five B here implies to never match the condition,
+ * as there are only four cells. In the case we do not give the number of states
+ * (<code>B</code> only for example), it implicitly consider a coefficient of 1.
+ * This is what occurs in the first rule.</li>
+ * 
+ * <li><code>(0,0)=B & ((0,1)=A | (0,-1)=A) : C</code><br/>
+ * This line gives a more complex condition, using some basic operators :
+ * <code>&</code> and <code>|</code> indicate respectively AND and OR operators
+ * (with the classical priority of <code>&</code> over <code>|</code>), there
+ * are also the parenthesis <code>(...)</code> to force some priorities. With
+ * them, you can create quite complex rules as there is no limit to the level of
+ * parenthesis nor the number of operators authorized.</li>
+ * </ol>
+ * </p>
+ * 
+ * <h2>Cells</h2>
+ * 
+ * <p>
+ * The cells part gives the description of the initial state of the space,
+ * giving in particular the number of dimensions, their lengths and the initial
+ * state of each cell.
+ * </p>
+ * <p>
+ * For 1D and 2D spaces, it is quite easy to write the description :
+ * <ul>
+ * <li>1 dimension of N cells = one line of N states :
+ * 
+ * <pre>
+ * AAABBAAC
+ * </pre>
+ * 
+ * </li>
+ * <li>2 dimensions of NxM cells = M lines of N states :
+ * 
+ * <pre>
+ * AAABBAAC
+ * BCCABABC
+ * CCCCBBBB
+ * </pre>
+ * 
+ * </li>
+ * </ul>
+ * The first dimension is the row (from left to right), the second dimension is
+ * the columns (from top to bottom). The origin (all coordinates at 0) is the
+ * top-left cell. Keep in mind you can only describe regular spaces, for example
+ * these descriptions are not authorized :
+ * <ul>
+ * <li>
+ * 
+ * <pre>
+ * AAABBAAC
+ * BCCABAB
+ * CCCCBB
+ * </pre>
+ * 
+ * </li>
+ * <li>
+ * 
+ * <pre>
+ * AAABBAAC
+ * BCCABABC
+ * CC    BB
+ * </pre>
+ * 
+ * </li>
+ * </ul>
+ * </p>
+ * <p>
+ * The description can have more dimensions. In order to describe them, you have
+ * to use separators. Any character which is not a state can be used as a
+ * separator, for example in a 3D description :
+ * 
+ * <pre>
+ * AAA|ABC
+ * BBB|CBA
+ * </pre>
+ * 
+ * The first and second dimensions are always the same, the third dimension
+ * select the left part (coordinate 0) or the second part (coordinate 1). You
+ * can add more like this :
+ * 
+ * <pre>
+ * AAA|ABC|ACB|CAB
+ * BBB|CBA|BAC|BCA
+ * </pre>
+ * 
+ * which means we have a first dimension of length 3 (each row of states), a
+ * second dimension of length 2 (the columns) and a third dimension of length 4
+ * (the blocks).
+ * </p>
+ * <p>
+ * You can use more dimensions, using another separator (here the character +) :
+ * 
+ * <pre>
+ * AAA|ABC
+ * BBB|CBA
+ * +++++++
+ * ACB|CAB
+ * BAC|BCA
+ * </pre>
+ * 
+ * Continuing with the same logic that previously, the fourth dimension give the
+ * choice between the higher part (coordinate 0) and the lower part (coordinate
+ * 1). As a reminder, the top-left A as the coordinates
+ * <code>(0, 0, 0, 0)</code> and the bottom-right A have
+ * <code>(2, 1, 1, 1)</code> (the first 2 is because the first dimension has a
+ * length of 3).
+ * </p>
+ * <p>
+ * You can add a lot more of dimensions this way (there is no limit), but you
+ * can format it in several ways. Until now, we have seen a way were we consider
+ * the dimensions in an horizontal way (the row), then a vertical way (the
+ * column), then horizontal (left part & right part), then vertical (top part &
+ * bottom part). This is the basic reading when you write such a <i>balanced</i>
+ * description. But you can write it in a row :
+ * 
+ * <pre>
+ * AAA|ABC+ACB|CAB
+ * BBB|CBA+BAC|BCA
+ * </pre>
+ * 
+ * You can also separate the first and the second dimension writting all the
+ * description in a single row :
+ * 
+ * <pre>
+ * AAA.BBB | ABC.CBA + ACB.BAC | CAB.BCA
+ * </pre>
+ * 
+ * and you can also do it in the vertical way.
+ * </p>
+ * <p>
+ * Finally, the parsing rules to recognize the dimensions are :
+ * <ol>
+ * <li>As long as we have a mixed horizontal+vertical description, the
+ * dimensions are ordered switching horizontal and vertical reading (starting by
+ * the horizontal way).</li>
+ * <li>When only one direction remains (whatever it is horizontal or vertical),
+ * the dimensions are ordered in the same order they are discovered
+ * (right-to-left or top-to-bottom).</li>
+ * </ol>
+ * </p>
  * 
  * @author Matthieu Vergne (matthieu.vergne@gmail.com)
  * 
@@ -46,20 +260,19 @@ public class ScriptSpaceBuilder {
 	private int height;
 	private Object[] characterSpace;
 
+	/**
+	 * Create a new builder.
+	 */
 	public ScriptSpaceBuilder() {
 		// do nothing
 	}
 
 	/**
-	 * This method allows to create a space as described in a textual file. This
-	 * file should follow this syntax :<br/>
-	 * 
-	 * <pre>
-	 * TODO describe syntax + make Javadoc
-	 * </pre>
+	 * This method is equivalent to {@link #createSpaceFromString(String)},
+	 * excepted it takes the description from a file.
 	 * 
 	 * @param filePath
-	 *            the path to the building file
+	 *            the path to the script file
 	 * @return the created space
 	 */
 	public void createSpaceFromFile(File file) {
@@ -93,6 +306,13 @@ public class ScriptSpaceBuilder {
 		createSpaceFromString(description);
 	}
 
+	/**
+	 * Create the space giving a description.You can have a description of the
+	 * syntax reading the documentation of {@link ScriptSpaceBuilder}.
+	 * 
+	 * @param description
+	 *            the description to consider
+	 */
 	public void createSpaceFromString(String description) {
 		try {
 			width = 0;
@@ -567,10 +787,18 @@ public class ScriptSpaceBuilder {
 		}
 	}
 
+	/**
+	 * 
+	 * @return the state factory used by the builder
+	 */
 	public IStateFactory<Character> getStateFactory() {
 		return builder.getStateFactory();
 	}
 
+	/**
+	 * 
+	 * @return the created space
+	 */
 	public ISpace<Character> getSpaceOfCell() {
 		return builder.getSpaceOfCell();
 	}
